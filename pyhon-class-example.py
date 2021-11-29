38e34a16ea13059c5e6f74b51b7a3dd9f4ebb339
Abi <contatoabigail_lopes@outlook.com>
2021-11-29 14:50:56 -0300

    Add a python class example

---
+++ pyhon-class-example.py
@@ -0,0 +1,16 @@
+class Car:
+    pass
+
+mein_wagen = Car()
+deine_wagen = Car()
+
+mein_wagen.year = 1996
+mein_wagen.model = 'Ford KA'
+mein_wagen.color = 'red'
+
+deine_wagen.year = 1991
+deine_wagen.model = 'Brasilia'
+deine_wagen.color = 'yellow'
+
+new_wagen = mein_wagen
+
